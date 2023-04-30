e96533e8a23291a5616efae21a59c1d7ddb6d6d1
howard <howard.hung@tpisoftware.com>
2023-04-30 08:30:36 +0800

    feat:excel
    調整項目:
    1.新增excel 範例dto

---
+++ src/main/java/com/example/utils/model/dto/DataDTO.java
@@ -0,0 +1,8 @@
+package com.example.utils.model.dto;
+
+import lombok.Data;
+
+@Data
+public class DataDTO {
+    private String data;
+}
---
+++ src/main/java/com/example/utils/model/dto/excel/ExcelExampleExportDTO.java
@@ -0,0 +1,9 @@
+package com.example.utils.model.dto.excel;
+
+import lombok.Data;
+
+@Data
+public class ExcelExampleExportDTO {
+    private String id;
+    private String name;
+}
