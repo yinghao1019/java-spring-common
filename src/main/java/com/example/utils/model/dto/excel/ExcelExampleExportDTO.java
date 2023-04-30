6b162ee0660011134f5a6120469ff3d344baab22
howard <howard.hung@tpisoftware.com>
2023-04-30 15:41:18 +0800

    feat: excel model dto
    調整項目:
    1.調整excel 範例model

--- src/main/java/com/example/utils/model/dto/excel/ExcelExampleExportDTO.java
+++ src/main/java/com/example/utils/model/dto/excel/ExcelExampleExportDTO.java
@@ -1,9 +1,12 @@
 package com.example.utils.model.dto.excel;
 
+import com.example.utils.spreedsheet.ExcelColumn;
 import lombok.Data;
 
 @Data
 public class ExcelExampleExportDTO {
+    @ExcelColumn(colIndex = 0, colName = "id")
     private String id;
+    @ExcelColumn(colIndex = 1, colName = "name")
     private String name;
 }
